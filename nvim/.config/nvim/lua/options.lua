f1edc2e1f05cffcbe6235e949e039f184b646ea2
jakozbek <jesse@polysign.io>
2023-01-08 09:21:46 -0600

    Annotates the options page

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,34 +1,47 @@
--- Default sets
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.errorbells = true
+-- Default Options
 
+-- Show Line Numbers
+vim.opt.number = true
+-- Show Relative Line Numbers
+vim.opt.relativenumber = true
+
+-- Only tab 4 (8 is default)
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+-- Number of spaces for autoindent
 vim.opt.shiftwidth = 4
+-- Use appropriate number of spaces from insert mode
 vim.opt.expandtab = true
-
+-- Smart indent when starting new lines
 vim.opt.smartindent = true
 
+-- Don't use a swapfile
 vim.opt.swapfile = false
+-- Don't use a backip file
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Use an undofile
 vim.undofile = true
+-- Store undo history directory
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
+-- Don't persist highlight of search
 vim.opt.hlsearch = false
+-- Show pattern matches as you type
 vim.opt.incsearch = true
 
+-- Show more colors
 vim.opt.termguicolors = true
 
+-- Buffer at least 8 lines above and below when scrolling
 vim.opt.scrolloff = 8
 
-vim.opt.updatetime = 50
+-- Update faster
+vim.opt.updatetime = 250
 
+-- Column to indicate line length
 vim.opt.colorcolumn = "100"
 
-vim.opt.smartcase = true
-vim.opt.showmatch = true
-vim.opt.pyxversion = 3
-
+-- Splits below for vertical splits
 vim.opt.splitbelow = true
+-- Splits above for horizontal splits
 vim.opt.splitright = true
