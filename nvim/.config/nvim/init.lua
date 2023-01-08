df0dab8aa818f39adc556454f06481c52e678d80
jakozbek <jesse@polysign.io>
2023-01-08 11:26:11 -0600

    Formatting applied

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,37 +1,28 @@
 -- Entrypoint
-require('options')
-require('mappings')
-require('work')
-require('plugins')
-
-----------------
--- Autoformat --
-----------------
-
--- TODO: what's going wrong with this and lua files
--- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format {async = true}]]
+require("options")
+require("mappings")
+require("work")
+require("plugins")
 
 -- Removes whitespaces on save
-vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = '*',
-    command = [[%s/\s\+$//e]]
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
 -- Github Copilot
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")',
-    { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 -----------------------
 -----------------------
 -- Wrap on .md files --
-local group = vim.api.nvim_create_augroup("Markdown Wrap Settings",
-    { clear = true })
+local group = vim.api.nvim_create_augroup("Markdown Wrap Settings", { clear = true })
 
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = { '*.md' },
-    group = group,
-    command = 'setlocal wrap'
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.md" },
+	group = group,
+	command = "setlocal wrap",
 })
 -- Wrap on .md files --
 -----------------------
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,249 +1,261 @@
 -- TODO: this kind of works, but a lot of errors are still hit on the initial load
 -- For bootstrapping packer
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 -- Executes packer compile whenever file change is made
-vim.api.nvim_create_augroup('packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = 'lua/plugins/config/*.lua,plugins.lua',
-    group = 'packer',
-    command = [[source lua/plugins.lua | PackerCompile]]
+vim.api.nvim_create_augroup("packer", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "lua/plugins/config/*.lua,plugins.lua",
+	group = "packer",
+	command = [[source lua/plugins.lua | PackerCompile]],
 })
 
-return require('packer').startup(function(use)
-    -- Package manager
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Package manager
+	use("wbthomason/packer.nvim")
 
-    -- LSP Related --
-    -----------------
-    -----------------
+	-- LSP Related --
+	-----------------
+	-----------------
 
-    -- LSP Configuration & Plugins
-    use {
-        'neovim/nvim-lspconfig',
-        requires = {
-            -- Automatically install LSPs to stdpath for neovim
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
+	-- Should be called before lsp
+	-- Null-ls --
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("plugins.config.null-ls")
+		end,
+	})
 
-            -- Additional lua configuration, makes nvim stuff amazing
-            'folke/neodev.nvim',
-        },
-        config = function()
-            -- neodev must be called before settings up lsp config
-            require('neodev').setup()
-            require('lsp-config')
-        end
-    }
+	-- LSP Configuration & Plugins
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-    -- Autocompletion
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-    }
+			-- Additional lua configuration, makes nvim stuff amazing
+			"folke/neodev.nvim",
+		},
+		config = function()
+			require("lsp-config")
+		end,
+	})
 
-    -- Null-ls --
-    use {
-        'jose-elias-alvarez/null-ls.nvim',
-        config = function()
-            require('plugins.config.null-ls')
-        end
-    }
+	-- Autocompletion
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+	})
 
-    -- Used to display Statusline from LSP in corner
-    use { 'j-hui/fidget.nvim', config = function() require "fidget".setup {} end }
+	-- Used to display Statusline from LSP in corner
+	use({
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup({})
+		end,
+	})
 
-    -- get icons for functions, etc. in LSP completion
-    use 'onsails/lspkind-nvim'
+	-- get icons for functions, etc. in LSP completion
+	use("onsails/lspkind-nvim")
 
-    use({
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-            vim.diagnostic.config({ virtual_text = false })
-            vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle,
-                { desc = "Toggle lsp_lines" })
-            require("lsp_lines").setup()
-        end
-    })
+	use({
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			vim.diagnostic.config({ virtual_text = false })
+			vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+			require("lsp_lines").setup()
+		end,
+	})
 
-    -----------------
-    -----------------
-    -- LSP Related --
+	-----------------
+	-----------------
+	-- LSP Related --
 
-    -- TODO: test this
-    use {
-        'lewis6991/impatient.nvim',
-        config = function()
-            pcall(require, 'impatient')
-        end
-    }
+	-- TODO: test this
+	use({
+		"lewis6991/impatient.nvim",
+		config = function()
+			pcall(require, "impatient")
+		end,
+	})
 
-    -- Start Page --
-    use { 'goolord/alpha-nvim',
-        config = function()
-            local default_config = require 'alpha.themes.dashboard'.config
+	-- Start Page --
+	use({
+		"goolord/alpha-nvim",
+		config = function()
+			local default_config = require("alpha.themes.dashboard").config
 
-            require 'alpha'.setup(default_config)
-        end,
-    }
+			require("alpha").setup(default_config)
+		end,
+	})
 
-    -- Treesitter --
-    use { -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter', --TODO: cool TS stuff from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
-        run = function()
-            pcall(require('nvim-treesitter.install').update { with_sync = true })
-        end,
-        config = function()
-            require('plugins.config.treesitter')
-        end
-    }
+	-- Treesitter --
+	use({ -- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter", --TODO: cool TS stuff from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+		run = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+		config = function()
+			require("plugins.config.treesitter")
+		end,
+	})
 
-    use { -- Additional text objects via treesitter
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-    }
+	use({ -- Additional text objects via treesitter
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+	})
 
-    -- File Tree --
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons' -- optional, for file icon
-        },
-        config = function()
-            require('plugins.config.nvim-tree')
-        end
-    }
+	-- File Tree --
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+		config = function()
+			require("plugins.config.nvim-tree")
+		end,
+	})
 
-    -- Toggle an area to view all current errors and warnings
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function() require("trouble").setup {} end
-    }
+	-- Toggle an area to view all current errors and warnings
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({})
+		end,
+	})
 
-    ----------
-    -- Rust --
+	----------
+	-- Rust --
 
-    -- Rust Additional Tools For LSP
-    use 'simrat39/rust-tools.nvim'
+	-- Rust Additional Tools For LSP
+	use("simrat39/rust-tools.nvim")
 
-    -- Crates
-    use {
-        'saecki/crates.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
-        config = function() require('crates').setup() end
-    }
+	-- Crates
+	use({
+		"saecki/crates.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup()
+		end,
+	})
 
-    -- Rust --
-    ----------
+	-- Rust --
+	----------
 
-    -- Debugging
-    use 'nvim-lua/plenary.nvim'
-    use 'mfussenegger/nvim-dap'
+	-- Debugging
+	use("nvim-lua/plenary.nvim")
+	use("mfussenegger/nvim-dap")
 
-    -- Commenting
-    use { 'numToStr/Comment.nvim',
-        config = function()
-            require('plugins.config.commenter')
-        end
-    }
+	-- Commenting
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("plugins.config.commenter")
+		end,
+	})
 
-    use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require('plugins.config.gitsigns')
-        end
-    }
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("plugins.config.gitsigns")
+		end,
+	})
 
-    -- Git
-    use 'tpope/vim-fugitive'
+	-- Git
+	use("tpope/vim-fugitive")
 
-    -- File Searching
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-            { 'nvim-lua/plenary.nvim' },
-            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-        },
-        config = function()
-            require('plugins.config.telescope')
-        end
-    }
+	-- File Searching
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+		},
+		config = function()
+			require("plugins.config.telescope")
+		end,
+	})
 
-    ---------
-    -- QOL --
-    ---------
+	---------
+	-- QOL --
+	---------
 
-    -- Surround --
-    use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-                -- Conflicts with lightspeed.nvim
-                keymaps = { visual = "Z" }
-            })
-        end
-    })
+	-- Surround --
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+				-- Conflicts with lightspeed.nvim
+				keymaps = { visual = "Z" },
+			})
+		end,
+	})
 
-    -- Motions --
-    use { 'ggandor/lightspeed.nvim',
-        config = function()
-            require('plugins.config.lightspeed')
-        end
-    }
+	-- Motions --
+	use({
+		"ggandor/lightspeed.nvim",
+		config = function()
+			require("plugins.config.lightspeed")
+		end,
+	})
 
-    use 'raimondi/delimitmate' -- for auto closing {}, (), "", etc.
+	use("raimondi/delimitmate") -- for auto closing {}, (), "", etc.
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-        config = function()
-            require('plugins.config.lualine')
-        end
-    }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("plugins.config.lualine")
+		end,
+	})
 
-    -- Which key, never for get a mapping!
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            require('plugins.config.whichkey')
-        end
-    }
+	-- Which key, never for get a mapping!
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("plugins.config.whichkey")
+		end,
+	})
 
-    -- Themes
-    use { "catppuccin/nvim", as = "catppuccin",
-        config = function()
-            require('plugins.config.catppuccin')
-        end
-    }
+	-- Themes
+	use({
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			require("plugins.config.catppuccin")
+		end,
+	})
 
-    -- Github Copilot
-    use 'github/copilot.vim'
+	-- Github Copilot
+	use("github/copilot.vim")
 
-    -- Orgmode nvim
-    use {
-        'nvim-orgmode/orgmode',
-        config = function()
-            require('plugins.config.org')
-        end,
+	-- Orgmode nvim
+	use({
+		"nvim-orgmode/orgmode",
+		config = function()
+			require("plugins.config.org")
+		end,
+	})
 
-    }
-
-
-    -- Used at bootstrap
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Used at bootstrap
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
