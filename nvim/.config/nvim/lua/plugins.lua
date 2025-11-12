50c28b68a11fb792db881c7031a8bc87a21f92c2
jakozbek <16109785+jakozbek@users.noreply.github.com>
2025-11-12 14:54:27 -0600

    Adds opencode integration to nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -364,6 +364,45 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"NickvanDyke/opencode.nvim",
+		dependencies = {
+			-- Recommended for `ask()` and `select()`.
+			-- Required for `snacks` provider.
+			---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+			{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		},
+		config = function()
+			---@type opencode.Opts
+			vim.g.opencode_opts = {
+				-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+			}
+
+			-- Required for `opts.auto_reload`.
+			vim.o.autoread = true
+
+			-- Recommended/example keymaps.
+			vim.keymap.set({ "n", "x" }, "<leader>A", function()
+				require("opencode").ask("@this: ", { submit = true })
+			end, { desc = "Ask opencode" })
+			vim.keymap.set({ "n", "x" }, "<C-x>", function()
+				require("opencode").select()
+			end, { desc = "Execute opencode action…" })
+			vim.keymap.set({ "n", "x" }, "ga", function()
+				require("opencode").prompt("@this")
+			end, { desc = "Add to opencode" })
+			vim.keymap.set({ "n", "t" }, "<leader>oc", function()
+				require("opencode").toggle()
+			end, { desc = "Toggle opencode" })
+			vim.keymap.set("n", "<S-C-u>", function()
+				require("opencode").command("session.half.page.up")
+			end, { desc = "opencode half page up" })
+			vim.keymap.set("n", "<S-C-d>", function()
+				require("opencode").command("session.half.page.down")
+			end, { desc = "opencode half page down" })
+		end,
+	},
+
 	------------------
 	-- Colorschemes --
 
@@ -384,14 +423,6 @@ require("lazy").setup({
 		end,
 	},
 
-	-- CSV Support
-	-- {
-	-- 	"chrisbra/csv.vim",
-	-- 	ft = { "csv" },
-	-- 	config = function()
-	-- 		vim.g.csv_no_conceal = 1
-	-- 	end,
-	-- },
 	{
 		"cameron-wags/rainbow_csv.nvim",
 		config = true,
